4c49a296f109095e914969dbeaa6cbc3e6ff11c3
lyslim <lyslim@gmail.com>
2011-12-06 00:03:54 +0800

    12.6

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -194,4 +194,5 @@ function add_admin_user(link, association,content){
 	var new_index = new Date().getTime();
 	var regexp = new RegExp("new_" + association, "g")
 	$(link).before(content.replace(regexp, new_index));
+	$(link).focus();
 }
